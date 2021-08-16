b56f9f2ce743fb40c40ba08ce3e8ab6bf8d8b4e9
yunusnode <yunusyagis@aof.anadolu.edu.tr>
2021-08-16 20:44:37 +0300

    Update StyledButton.tsx

--- src/components/Button/StyledButton.tsx
+++ src/components/Button/StyledButton.tsx
@@ -47,11 +47,11 @@ const getButtonVariantProp = (prop: keyof ButtonThemeVariant) => ({
 
 const StyledButton = styled.button<ButtonProps>`
   align-items: center;
-  background-color: ${getButtonVariantProp("background")};
+  background-color: #c09a5b;
   border: ${getButtonVariantProp("border")};
   border-radius: 16px;
   box-shadow: ${getButtonVariantProp("boxShadow")};
-  color: ${getButtonVariantProp("color")};
+  color: #bfc0bf;
   cursor: pointer;
   display: inline-flex;
   font-family: inherit;
